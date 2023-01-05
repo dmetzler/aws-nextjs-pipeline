dad05e28bf690e4c38ce4e460b80ca6a89c3b068
Damien Metzler <dmetzler@amazon.fr>
2023-01-05 19:15:09 +0100

    fix test

--- test/aws-nextjs-pipeline.test.ts
+++ test/aws-nextjs-pipeline.test.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as AwsNextjsPipeline from '../lib/aws-nextjs-pipeline-stack';
+import * as AwsNextjsPipeline from '../lib/nextjs-stack';
 
 test('SQS Queue and SNS Topic Created', () => {
   const app = new cdk.App();
