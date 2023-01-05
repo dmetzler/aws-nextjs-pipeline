2ea710ed2fbf236142c963c4fcdd001e42402192
Damien Metzler <dmetzler@amazon.fr>
2023-01-05 18:50:58 +0100

    init codepipeline

---
+++ bin/cdk-nextjs-pipeline.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { PipelineStack } from '../lib/pipeline-stack';
+
+const app = new cdk.App();
+new PipelineStack(app, 'NextjsPipelineStack');
---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,34 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
+
+
+export class PipelineStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+
+        
+        // The basic pipeline declaration. This sets the initial structure
+        // of our pipeline
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'NextJSPipeline',
+            synth: new CodeBuildStep('SynthStep', {
+                input: CodePipelineSource.connection('dmetzler/aws-nextjs-pipeline','main',{
+                    connectionArn:'arn:aws:codestar-connections:eu-west-1:039665761580:connection/0ae15fc7-c2e4-4265-ab52-6d97da2aef92'
+                }),
+                installCommands: [
+                    'npm install -g aws-cdk'
+                ],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ]
+            }
+            )
+        });
+
+     
+    }
+}
