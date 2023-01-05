479f66bab44e78a22b697f562f09394fc12a6fb2
Damien Metzler <dmetzler@amazon.fr>
2023-01-05 19:28:55 +0100

    fix test

--- test/aws-nextjs-pipeline.test.ts
+++ test/aws-nextjs-pipeline.test.ts
@@ -1,11 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as AwsNextjsPipeline from '../lib/nextjs-stack';
+import * as NextjsStack from '../lib/nextjs-stack';
 
 test('SQS Queue and SNS Topic Created', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new AwsNextjsPipeline.AwsNextjsPipelineStack(app, 'MyTestStack');
+  const stack = new NextjsStack.NextjsStack(app, 'MyTestStack');
   // THEN
 
   const template = Template.fromStack(stack);
